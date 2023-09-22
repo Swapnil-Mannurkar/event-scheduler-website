a49bab819267a54085aa9a820a9ab9ab56151235
Swapnil Mannurkar <Mannurkarswapnil@gmail.com>
2023-09-22 12:27:22 +0530

    commit

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import styles from "../../styles/Navbar.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
+
   return (
     <nav className={styles.navbarContainer}>
       <ul className={styles.list}>
-        <Link href="/">HOME</Link>
-        <Link href="/events">EVENTS</Link>
-        <Link href="/new-events">NEW EVENT</Link>
+        <Link
+          href="/"
+          className={router.pathname === "/" && `${styles.active}`}
+        >
+          HOME
+        </Link>
+        <Link
+          href="/events"
+          className={router.pathname === "/events" && `${styles.active}`}
+        >
+          EVENTS
+        </Link>
+        <Link
+          href="/new-events"
+          className={router.pathname === "/new-events" && `${styles.active}`}
+        >
+          NEW EVENT
+        </Link>
       </ul>
     </nav>
   );
